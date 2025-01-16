215feade13471701dfa61b1dc1991bc0238131a7
Annie Chen <a.janice.chen@gmail.com>
2025-01-16 15:03:30 +0100

    feat: add set up prim and sec styling

---
+++ app/src/components/atoms/button/Component.tsx
@@ -0,0 +1,13 @@
+import styles from "./style.module.css"
+interface Props {
+    children?: string
+    type: "primary" | "secondary"
+}
+
+export default function Button(props: Props) {
+    const styleCheck = props.type === 'secondary' ? styles.secondary : styles.primary
+
+    return (
+        <a className={styleCheck}>{props.children}</a>
+    );
+  }
