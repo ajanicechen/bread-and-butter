25a695155fc0a6fec10a753e8ac65852f519bbaf
Annie Chen <a.janice.chen@gmail.com>
2025-01-18 14:19:23 +0100

    feat: add antique and parabole fontface

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "JUST | Bread and butter",
@@ -24,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <h1>Hi</h1>
+        <p>Hi</p>
         {children}
       </body>
     </html>
