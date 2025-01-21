2fb1c10d49d97f789af943c9ecd1df8a8671f72c
Annie Chen <a.janice.chen@gmail.com>
2025-01-21 14:18:09 +0100

    feat: add footer component

---
+++ app/src/components/molecules/footer/Component.tsx
@@ -0,0 +1,24 @@
+import InputField from "../inputField/Component";
+import Styles from "./Style.module.css";
+
+export default function Footer() {
+    return (
+        <div className={Styles.wrapper}>
+            <div className={Styles.footer}>
+                <h1>Join our next events</h1>
+                <p>Do you want to join our next Bread & Butter event? Leave your email and we’ll keep you up to date.</p>
+                <InputField />
+            </div>
+            <ul className={Styles.contact}>
+                <li className={Styles['contact-item']}>
+                    <a href="mailto:info@voorbeeld.com" aria-label="Stuur een e-mail naar Just Agency">
+                        hello@just.agency
+                    </a>
+                </li>
+                <li className={Styles['contact-item']} >
+                    <a href="tel:+1234567890" aria-label="Bel Just Agency">+31 (0)88 587 8000</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
