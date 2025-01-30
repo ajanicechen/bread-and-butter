e6975dbbb03c9bffb12c2746ded4b1675e679025
Annie Chen <a.janice.chen@gmail.com>
2025-01-30 16:10:11 +0100

    fix: fix slider, aria-label, padding

--- app/src/components/molecules/navbar/Component.tsx
+++ app/src/components/molecules/navbar/Component.tsx
@@ -5,7 +5,7 @@ import JustLogo from "images/just-logo.svg"
 export default function Navbar() {
     return (
         <div className={Styles.navbar}>
-            <a href='https://just.agency/nl' target='_blank' className={Styles['logo-wrapper']}>
+            <a href='https://just.agency/nl' target='_blank' className={Styles['logo-wrapper']} aria-label="Just agency">
                 <JustLogo className={Styles.logo} />
             </a>
             <ThemeToggle />
