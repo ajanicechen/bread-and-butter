97c6c6555536e77e1cc62ac6118436f870d477b5
Annie Chen <a.janice.chen@gmail.com>
2025-02-05 20:51:21 +0100

    feat: add sticker to hero

--- app/src/components/molecules/hero/Component.tsx
+++ app/src/components/molecules/hero/Component.tsx
@@ -1,14 +1,18 @@
 'use client';
 
+import Sticker from "@/components/atoms/sticker/Component";
 import Styles from "./Style.module.css";
 import BnbLogo from "images/b&b-logo.svg";
 
 export default function Hero() {
   return (
     <div className={Styles.hero}>
-        <div className={Styles['logo-wrapper']}>
-          <BnbLogo className={Styles.logo}/>
-        </div>
+      <span className={Styles['sticker-wrapper']}>
+        <Sticker />
+      </span>
+      <div className={Styles['logo-wrapper']}>
+        <BnbLogo className={Styles.logo} />
+      </div>
     </div>
   );
 }
