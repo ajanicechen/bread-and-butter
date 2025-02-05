b21808989ae888c35d1da5ec534b74ccd9462edc
Annie Chen <a.janice.chen@gmail.com>
2025-02-05 20:19:29 +0100

    feat: add sticker svgs

---
+++ app/src/data/stickers.tsx
@@ -0,0 +1,19 @@
+import Percentage from 'images/stickers/100.svg'
+import Accessible from 'images/stickers/Accessible.svg'
+import Agenda from 'images/stickers/Agenda.svg'
+import Cheers from 'images/stickers/Cheers.svg'
+import CoffeeTime from 'images/stickers/Coffee_Time.svg'
+import EndMagic from 'images/stickers/End_with_Magic.svg'
+import Exclamations from 'images/stickers/exclamations.svg'
+import FeedbackPls from 'images/stickers/Feedback_Please.svg'
+
+export const stickers = [
+    Percentage, 
+    Accessible, 
+    Agenda, 
+    Cheers, 
+    CoffeeTime, 
+    EndMagic, 
+    Exclamations,
+    FeedbackPls,
+]
