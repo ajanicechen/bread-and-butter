0c996fcd80265286320637fc11260ce9ee48e2b0
Annie Chen <a.janice.chen@gmail.com>
2025-02-05 20:51:32 +0100

    feat: add sticker to footer

--- app/src/components/molecules/footer/Component.tsx
+++ app/src/components/molecules/footer/Component.tsx
@@ -1,13 +1,18 @@
+import Sticker from "@/components/atoms/sticker/Component";
 import InputField from "../inputField/Component";
 import Styles from "./Style.module.css";
 
 export default function Footer() {
     return (
         <div className={Styles.wrapper}>
+
             <div className={Styles.footer}>
                 <h1>Join our next events</h1>
                 <p>Do you want to join our next Bread & Butter event? Leave your email and we’ll keep you up to date.</p>
                 <InputField />
+                <span className={Styles['sticker-wrapper']}>
+                    <Sticker />
+                </span>
             </div>
             <ul className={Styles.contact}>
                 <li className={Styles['contact-item']}>
@@ -19,6 +24,7 @@ export default function Footer() {
                     <a href="tel:+1234567890" aria-label="Bel Just Agency">+31 (0)88 587 8000</a>
                 </li>
             </ul>
+
         </div>
     )
 }
